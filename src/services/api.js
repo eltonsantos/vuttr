55ec7e8db2f345647c6042fd0c5f5f4a7d89e58c
Elton Santos <eltonaxl@hotmail.com>
2021-06-11 21:52:24 -0300

    Fix URL in api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/eltonsantos/vuttr'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/'
 });
